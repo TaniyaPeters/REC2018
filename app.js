7405f1d50752bf19b21cbd186009b116991e0f4b
TaniyaPeters <tpeterratnaraj@gmail.ca>
2018-11-16 23:00:47 -0500

    Got Nodejs to switch tabs. Did some other stuff

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+var path = __dirname + "/views/"
 
 // Initialise express app
 const app = express();
@@ -10,5 +11,17 @@ let port = 8080;
 
 // Launch app
 app.listen(port, () => {
-    console.log('Server is up and running');
+  console.log('Server is up and running');
+});
+
+app.get('/', function(req,res){
+  res.sendFile(path + 'FoodBack.html');
+});
+
+app.get('/maps', function(req,res){
+  res.sendFile(path + 'Maps.html');
+});
+
+app.get('/info', function(req,res){
+  res.sendFile(path + 'Information.html');
 });
