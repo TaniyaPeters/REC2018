4983ce20f2fef4719572ba7f891b3daff1714253
Torquelus <alari.tork@gmail.com>
2018-11-16 22:28:09 -0500

    New app.js attempt

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-var http = require('http');
-var fs = require('fs');
-var mongo = require('mongodb');
+const http = require('http');
+const fs = require('fs');
+const mongo = require('mongodb');
 
 console.log("node.js running");
 
+// Create MongoDB DataBase
+const MongoClient = mongo.MongoClient;
+const url = "mongodb://localhost:27017/mydb";
+
+// Create DataBase
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
+// Create HTML
 http.createServer(function (req, res) {
     fs.readFile('views/FoodBack.html', function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
